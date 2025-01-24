b563f26476b393e6182cf74d4d9e3fafc5886fc4
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2025-01-24 18:50:21 +0600

    cors add

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all origins or specific origin (frontend URL)
+  app.enableCors({
+    origin: 'http://localhost:3001', // Allow only your frontend URL
+    methods: 'GET,POST,PUT,DELETE', // Allowed methods
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
